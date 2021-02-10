c61434b9eede02939f327f01ad0e6c1db6e00516
payerne <constantin.payerne@etu.univ-grenoble-alpes.fr>
2021-02-10 09:09:21 +0100

    add legends

--- modeling/.ipynb_checkpoints/two_halo_term-checkpoint.py
+++ modeling/.ipynb_checkpoints/two_halo_term-checkpoint.py
@@ -3,7 +3,13 @@ import pyccl as ccl
 from scipy.integrate import quad
 from scipy.special import jv
 
+"""
+    We compute the excess surface density profile due to the matter distribution of large scale structure surrounding halo;
+    Computation of power spectrum and halo bais by Core Cosmology Library
+"""
+
 def compute_Pk(kk, z, cosmo_ccl):
+    
     r"""
     Atributes:
     ---------
@@ -12,10 +18,12 @@ def compute_Pk(kk, z, cosmo_ccl):
     z : float
         cluster redshift
     cosmo_ccl : cosmology object from ccl
+    
     Returns:
     -------
     Pk : The computed linear matter power spectrum for each kk
     """
+    
     Pk = []
     
     for k in kk:
@@ -25,7 +33,10 @@ def compute_Pk(kk, z, cosmo_ccl):
     return np.array(Pk)
 
 def two_halo_term_unbaised(r, cluster_z, cosmo_ccl, kk, Pk):
+    
     r"""
+    based on (Oguri & Takada 2011 and Oguri & Hamana 2011, arXiv:1010.0744v2)
+    
     Attributes:
     ----------
     r : array, float
@@ -34,22 +45,19 @@ def two_halo_term_unbaised(r, cluster_z, cosmo_ccl, kk, Pk):
         cluster redshift
     cosmo_ccl : cosmology object from ccl
     kk, Pk : wavevector and corresponding linear matter power spectrum
+    
     Return:
     ------
-    
+    the excess surface density induced by large scale struture, divided by the halo bais
     """
     
     Da = ccl.angular_diameter_distance(cosmo_ccl, 1, 1./(1. + cluster_z))
     
     rho_m = ccl.rho_x(cosmo_ccl, 1./(1. + cluster_z), 'matter', is_comoving=False)
-    
-    def k_func( l ):
-        
-        return l / ( ( 1 + cluster_z ) * Da )
 
-    def __integrand__( l , theta ):
+    def integrand( l , theta ):
         
-        k = k_func( l )
+        k = l / ( ( 1 + cluster_z ) * Da )
         
         return l * jv( 2 , l * theta ) * np.interp( k , kk , Pk )
     
@@ -59,15 +67,39 @@ def two_halo_term_unbaised(r, cluster_z, cosmo_ccl, kk, Pk):
         
         theta = R/Da
     
-        val = quad( __integrand__  , 2*1e-4 , 20000 , args = ( theta ))[0]
+        val = quad(integrand  , 2*1e-4 , 20000 , args = ( theta ))[0]
+        
         two_h.append(val * rho_m / ( 2 * np.pi  * ( 1 + cluster_z )**3 * Da**2 ))
         
     return np.array(two_h)
 
 def halo_bais(logm = 1, mdef = 'matter', Delta = 200, cluster_z = 1, cosmo_ccl = 1):
     
+    r"""
+    Attributes:
+    ----------
+    logm : float, array
+        the log10 of the cluster mass
+    mdef : str
+        the overdensity mass definition
+    Delta : int
+        the overdensity relative to mass definition
+    cluster_z : float
+        the cluster redshift
+    cosmo_ccl: object
+        Cosmology object of CCL
+        
+    Returns:
+    -------
+    hbais : the Tinker (2010) halo bais for mass logm and overdensity definition
+    
+        (Jeremy L. Tinker et al., arXiv:1001.3162v2)
+    r"""
+    
     definition = ccl.halos.massdef.MassDef(Delta, mdef, c_m_relation=None)
     
     halobais = ccl.halos.hbias.HaloBiasTinker10(cosmo_ccl, mass_def=definition, mass_def_strict=True)
     
-    return halobais.get_halo_bias(cosmo_ccl, 10**logm, 1/(1+cluster_z), mdef_other = definition)
+    hbais = halobais.get_halo_bias(cosmo_ccl, 10**logm, 1/(1+cluster_z), mdef_other = definition)
+    
+    return hbais
